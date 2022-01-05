e36991f3722794e4451a304610f869dd6bf53592
Ulin Nuha <ulinnuha09basketball@gmail.com>
2022-01-05 23:00:31 +0700

    eror di index out of range

--- resep_makanan/resep_makanan/lib/views/detail.dart
+++ resep_makanan/resep_makanan/lib/views/detail.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:resep_makanan/model/resep.dart';
 import 'package:resep_makanan/views/widget/resep_card.dart';
 
 class DetailResep extends StatelessWidget {
@@ -8,6 +11,8 @@ class DetailResep extends StatelessWidget {
   final String totalTime;
   final String description;
   final String videoUrl;
+  final List<Instructions> instructions;
+  final List<Section> sections;
 
   DetailResep({
     required this.name,
@@ -16,7 +21,43 @@ class DetailResep extends StatelessWidget {
     required this.totalTime,
     required this.description,
     required this.videoUrl,
+    required this.instructions,
+    required this.sections,
   });
+
+  TextEditingController nama = TextEditingController();
+  TextEditingController coment = TextEditingController();
+  final firebase = FirebaseFirestore.instance;
+
+  create() async {
+    try {
+      await firebase
+          .collection("User")
+          .doc(nama.text)
+          .set({"nama": nama.text, "coment": coment.text});
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  update() async {
+    try {
+      firebase
+          .collection("User")
+          .doc(nama.text)
+          .update({'coment': coment.text});
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  delete() async {
+    try {
+      firebase.collection("User").doc(nama.text).delete();
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,40 +68,161 @@ class DetailResep extends StatelessWidget {
             children: [
               Icon(Icons.restaurant_menu),
               SizedBox(width: 10),
-              Text('Resep Makanan')
+              Text('Komentar Saya')
             ],
           ),
         ),
         body: SafeArea(
-          child: Column(
-            children: [
-              ResepCard(
-                title: name,
-                cookTime: totalTime,
-                rating: rating,
-                thumbnailUrl: images,
-                videoUrl: videoUrl,
-              ),
-              Container(
-                child: Column(
-                  children: [
-                    Text(
-                      'Description',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                      textAlign: TextAlign.left,
-                    ),
-                    Text(description,
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                        textAlign: TextAlign.justify)
-                  ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ResepCard(
+                  title: name,
+                  cookTime: totalTime,
+                  rating: rating,
+                  thumbnailUrl: images,
+                  videoUrl: videoUrl,
+                  /* instructions: [],
+                  sections: [], */
                 ),
-              )
-            ],
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                  child: Column(
+                    children: [
+                      TextField(
+                          controller: nama,
+                          decoration: InputDecoration(
+                              labelText: "Username",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ))),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextField(
+                        controller: coment,
+                        decoration: InputDecoration(
+                            labelText: "Komentar",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            )),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.green),
+                              onPressed: () {
+                                create();
+                                nama.clear();
+                                coment.clear();
+                              },
+                              child: Text("Create")),
+                          ElevatedButton(
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.amber),
+                              onPressed: () {
+                                update();
+                                nama.clear();
+                                coment.clear();
+                              },
+                              child: Text("Update")),
+                          ElevatedButton(
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.red),
+                              onPressed: () {
+                                delete();
+                                nama.clear();
+                                coment.clear();
+                              },
+                              child: Text("Delete")),
+                        ],
+                      ),
+                      Container(
+                        height: 300,
+                        width: double.infinity,
+                        child: SingleChildScrollView(
+                          physics: ScrollPhysics(),
+                          child: StreamBuilder<QuerySnapshot>(
+                              stream: firebase.collection("User").snapshots(),
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  return ListView.builder(
+                                    itemCount : 
+                                      shrinkWrap: true,
+                                      physics: ScrollPhysics(),
+                                      itemBuilder: (context, i) {
+                                        QueryDocumentSnapshot x =
+                                            snapshot.data!.docs[i];
+                                        return Text(x['nama']);
+                                      });
+                                } else {
+                                  return CircularProgressIndicator();
+                                }
+                              }),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+
+                /*  Container(
+                  margin: EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Description',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Disini adalah Deskripsi",
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.justify),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Instruction',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                          textAlign: TextAlign.left,
+                        ),
+                      ), */
+
+                /*  Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListView.builder(
+                            itemCount: instructions.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(instructions[index].displayText),
+                              );
+                            },
+                          )), 
+                    ], */
+                /*    ],
+                  ),
+                ) */
+              ],
+            ),
           ),
         ));
   }
